7ed32a0fce631105747423554bc11e0e0db91bad
newmac <bala223344@gmail.com>
2018-04-12 14:11:33 +0530

    active menu fix

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
+        
          <header className={`menu-containter ${this.props.customClass}`}>
             <div className="logo">
                 <a href="/">
@@ -18,29 +19,30 @@ class Header extends Component {
                         </a>
                     </div>
                     <ul className="menu-list">
-                        <li>
-                            <a href="/" className="active">home</a>
+
+                        <li >
+                            <a href="/" className={ (window.location.pathname == "/")? "active": "" }>home </a>
                         </li>
                         <li>
-                            <a href="/shop">Shop</a>
+                            <a href="/shop" className={ (window.location.pathname == "/shop")? "active": "" } >Shop</a>
+                        </li>
+                        <li >
+                            <a href="/send" className={ (window.location.pathname == "/send")? "active": "" }>Send</a>
                         </li>
                         <li>
-                            <a href="/send">Send</a>
+                            <a href="/trace" className={ (window.location.pathname == "/trace")? "active": "" }>Trace</a>
                         </li>
-                        <li>
-                            <a href="/trace">Trace</a>
+                        <li >
+                            <a href="/travel" className={ (window.location.pathname == "/travel")? "active": "" }>Travelers</a>
                         </li>
-                        <li>
-                            <a href="/travel">Travelers</a>
+                        <li >
+                            <a href="/agent" className={ (window.location.pathname == "/agent")? "active": "" }>Agent</a>
                         </li>
-                        <li>
-                            <a href="/agent">Agent</a>
+                        <li >
+                            <a href="/about" className={ (window.location.pathname == "/about")? "active": "" }>About Us</a>
                         </li>
-                        <li>
-                            <a href="/about">About Us</a>
-                        </li>
-                        <li>
-                            <a href="/contact-us">Contact</a>
+                        <li >
+                            <a href="/contact-us" className={ (window.location.pathname == "/contact-us")? "active": "" }>Contact</a>
                         </li>
                         <li className="bottom-btn hidden-lg hidden-md">
                             <div>
