918479fa116e6f4c3bc20c29cdc7f9e02878c466
Shashib <sbhushan@smartdatainc.net>
2018-04-27 18:04:59 +0530

    Login and signup API

--- src/components/login.js
+++ src/components/login.js
@@ -3,8 +3,43 @@ import React, { Component } from 'react';
 import MainCustomWrap from './layout/main-custom-wrap';
 import Header from './layout/header';
 import FooterOther from './layout/footer-other';
+import Parse from 'parse'
 
 class Login extends Component {
+
+    constructor(){
+        super();
+        Parse.initialize("kgMoNAcxUBHJIo9KUVQqcjYOZvpZv4fR4pky1zJH", "je5obIpZemmDCQ1ivZqcZaj7vpUNVFQtvjElbpAi"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
+        Parse.serverURL = "https://parseapi.back4app.com/";
+        this.state = {
+            userName:'',
+            password:'',
+        
+            
+
+        };
+    }
+    
+    onUsernameChange = (e) => {
+        this.setState({ userName: e.target.value });
+        
+    };
+    onPasswordChange = (e) => {
+        this.setState({ password: e.target.value });
+        
+    };
+    login(userData){
+        Parse.User.logIn(userData.userName, userData.password , {
+            success: function(user) {
+                alert("User logged in successful with username: " + user.get("username") + ' and email: ' + user.get("email"));
+                // Do stuff after successful login, like a redirect.
+            },
+            error: function(user, error) {
+                alert("The login failed with error: " + error.code + " " + error.message);
+            }
+        });
+    }
+
 	render() {
 		return (
             <MainCustomWrap customClass="login">
@@ -31,20 +66,36 @@ class Login extends Component {
                             <i>
                                 <img className="icon" src="images/icons/icons8-human-head-26.png" />
                             </i>
-                            <input type="text" className="" placeholder="Username..." />
+                            <input 
+                            type="text" 
+                            name = "username" 
+                            value = {this.state.userName } 
+                            onChange = { this.onUsernameChange }
+                            className="" 
+                            placeholder="Username..."  />
                         </div>
                         <div className="input">
                             <i>
                                 <img className="icon" src="images/icons/icons8-lock-24.png" />
                             </i>
-                            <input type="password" className="" placeholder="Password..." />
+                            <input 
+                            type="password" 
+                            name = "password" 
+                            value = { this.state.password }
+                            onChange = { this.onPasswordChange } 
+                            className="" 
+                            placeholder="Password..." />
                         </div>
                         <div className="text-right">
                             <a href="#" className="text-uppercase forget-password">Forget Password</a>
                         </div>
                         <div className="text-center">
                             <br/><br/>
-                            <input type="submit" value="Join" className="text-uppercase hollow-button" />
+                            <input 
+                            type="button" 
+                            onClick={()=>this.login(this.state)} 
+                            value="Join" 
+                            className="text-uppercase hollow-button" />
                         </div>
                     </form>
                 </div>
--- src/components/signup.js
+++ src/components/signup.js
@@ -1,9 +1,65 @@
-import React, { Component } from 'react';
-
+import React, { PureComponent } from 'react';
 import MainCustomWrap from './layout/main-custom-wrap';
 import Header from './layout/header';
+import Parse from 'parse'
 
-class Signup extends Component {
+class Signup extends  PureComponent {
+
+    constructor(){
+        super();
+        Parse.initialize("kgMoNAcxUBHJIo9KUVQqcjYOZvpZv4fR4pky1zJH", "je5obIpZemmDCQ1ivZqcZaj7vpUNVFQtvjElbpAi"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
+        Parse.serverURL = "https://parseapi.back4app.com/";
+        this.state ={
+            userName:'',
+            password:'',
+            email:'',
+            username:'',
+            birthday :''
+            
+
+        };
+    }
+    onUsernameChange = (e) => {
+        this.setState({ userName: e.target.value });
+        
+    };
+    onPasswordChange = (e) => {
+        this.setState({ password: e.target.value });
+        
+    };
+    onEmailChange = (e) => {
+        this.setState({ email: e.target.value });
+        
+    };
+    onBirthdayChange = (e) => {
+        this.setState({ birthday: e.target.value });
+        
+    };
+    signUp(userData) {
+        // Create a new instance of the user class
+        console.log('user is:',userData);
+        let user = new Parse.User();
+        /*user.set("username", "my name");
+        user.set("password", "my pass");
+        user.set("email", "email@example.com");
+        */
+       user.set("username",userData.userName );
+       user.set("password", userData.password);
+       user.set("email", userData.email);
+       user.set("birthday", userData.birthday);
+    
+        user.signUp(null, {
+          success: function(user) {
+            alert('User created successful with name: ' + user.get("username") + ' and email: ' + user.get("email"));
+          },
+          error: function(user, error) {
+            // Show the error message somewhere and let the user try again.
+            // It is likely that the user is trying to sign with a username or email already taken.
+            alert("Error: " + error.code + " " + error.message);
+          }
+        });
+    }
+
 	render() {
 		return (
             <MainCustomWrap customClass="login signup">
@@ -37,28 +93,54 @@ class Signup extends Component {
                             <i>
                                 <img className="icon" src="images/icons/icons8-human-head-26.png" />
                             </i>
-                            <input type="text" className="" placeholder="Username..." />
+                            <input
+                                name = "username" 
+                                value = {this.state.userName } 
+                                onChange = { this.onUsernameChange }
+                                type="text" className="" 
+                                placeholder="Username..." 
+                             />
                         </div>
                         <div className="input">
                             <i>
                                 <img className="icon" src="images/icons/icons8-envelope-24.png" />
                             </i>
-                            <input type="email" className="" placeholder="Email..." />
+                            <input 
+                                type="email"
+                                name = "email"
+                                value = {this.state.email} 
+                                onChange = { this.onEmailChange }
+                                className=""
+                                placeholder="Email..." 
+                            />
                         </div>
                         <div className="input">
                             <i>
                                 <img className="icon" src="images/icons/icons8-lock-24.png" />
                             </i>
-                            <input type="password" className="" placeholder="Password..." />
+                            <input 
+                            type="password" 
+                            name = "password" 
+                            value = { this.state.password }
+                            onChange = { this.onPasswordChange } 
+                            className="" 
+                            placeholder="Password..." />
                         </div>
                         <div className="input">
                             <i>
                                 <img className="icon" src="images/icons/icons8-gift-24.png" />
                             </i>
-                            <input type="text" className="" placeholder="Birthday..." />
+                            <input 
+                                type="text" 
+                                name = "birthday" 
+                                value = {this.state.birthday} 
+                                onChange = { this.onBirthdayChange }
+                                className="" 
+                                placeholder="Birthday..." 
+                            />
                         </div>
                         <div className="text-center">
-                            <input type="submit" value="Get Started" className="text-uppercase hollow-button" styles="margin-top: 0;" />
+                            <input type="button" onClick={()=> this.signUp(this.state)} value="Get Started" className="text-uppercase hollow-button" styles="margin-top: 0;" />
                         </div>
                     </form>
                 </div>
